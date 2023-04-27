7b82f1ba5c7cf07ac2caef3bee5de41f12488300
SnowflakeCoDev <115969762+SnowflakeCoDev@users.noreply.github.com>
2023-04-27 12:31:56 +0200

    Update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "ad326047cdc1034de3f22901158168f6",
-"index.html": "81d5faffa6a8da6b067726b56671e49d",
-"/": "81d5faffa6a8da6b067726b56671e49d",
-"main.dart.js": "1a8c0dbd6abfbd3ac3eb8f21d739da06",
+"index.html": "43bd1b229adb5b89218c060520790937",
+"/": "43bd1b229adb5b89218c060520790937",
+"main.dart.js": "0b2a3596923aa064d012e26bef691197",
 "flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
