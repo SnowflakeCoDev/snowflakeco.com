eba692706e5b13309fcba26f06d95896b0b2e3e3
SnowflakeCoDev <115969762+SnowflakeCoDev@users.noreply.github.com>
2023-05-25 09:47:16 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "ad326047cdc1034de3f22901158168f6",
-"index.html": "f9c2ed47141ac0d06bfc2757a2ea1820",
-"/": "f9c2ed47141ac0d06bfc2757a2ea1820",
-"main.dart.js": "6cb58bceff8b7906bc10d6e9a73be8b8",
+"index.html": "68437349497a09c610d96249f136effe",
+"/": "68437349497a09c610d96249f136effe",
+"main.dart.js": "c1565c430d44cde8ef98a2e21cb73c19",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
