902c94b5db55d4c8ff39266c48247d243fa4c1ff
waleed hassanin <snowflakecodev12022@gmail.com>
2023-11-23 07:40:47 +0200

    Update

--- dashboard/snowflake/flutter_service_worker.js
+++ dashboard/snowflake/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "7bd46033a147c8d33a1d572af3ad7af4",
-"index.html": "a713c9af5b1999666e851c7e5eb125a2",
-"/": "a713c9af5b1999666e851c7e5eb125a2",
-"main.dart.js": "9ae8a4d36b31c173cdc9535ff87cc703",
+"index.html": "6fe40440f4ff3d02f27dfdb22eaa8435",
+"/": "6fe40440f4ff3d02f27dfdb22eaa8435",
+"main.dart.js": "6e1d4cc23eb15b82cdf36f564c9cbaf8",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "ad326047cdc1034de3f22901158168f6",
-"index.html": "0ea824b5de209f463c2d90732f63f777",
-"/": "0ea824b5de209f463c2d90732f63f777",
-"main.dart.js": "abfd3ec7db11a3dbada3f912852094de",
+"index.html": "53fc91259c0d3684851b6e54a5f19ba1",
+"/": "53fc91259c0d3684851b6e54a5f19ba1",
+"main.dart.js": "2ef22e132a71f001410289553b0d5671",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
