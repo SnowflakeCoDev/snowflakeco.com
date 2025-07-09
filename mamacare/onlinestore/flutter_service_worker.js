dee5af92fb0eecc3f344b81b020030b8584f1e6b
Waleed Hassanin <115969762+SnowflakeCoDev@users.noreply.github.com>
2025-07-09 12:17:38 +0300

    Update

--- mamacare/onlinestore/flutter_service_worker.js
+++ mamacare/onlinestore/flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "1b21d0bc6f395a8bc6ac297ea272da80",
+const RESOURCES = {"flutter_bootstrap.js": "b8d8eee9ac9f7caee76fc2e87168d309",
 "version.json": "0589e34cd81e7db5bc97f5025286fbb9",
-"index.html": "1952352b237d56620a354364ff2d6c55",
-"/": "1952352b237d56620a354364ff2d6c55",
+"index.html": "7758a8a28f19b40ab6df55d31f2f44a1",
+"/": "7758a8a28f19b40ab6df55d31f2f44a1",
 "main.dart.js": "7f73ed502f2946f3cb03035c70cb33c5",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
