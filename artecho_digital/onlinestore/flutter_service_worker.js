9ddd7b2921dd128d868d0698783e4dcfc59950e9
waleed hassanin <snowflakecodev12022@gmail.com>
2025-10-09 14:45:43 +0300

    Update

--- artecho_digital/onlinestore/flutter_service_worker.js
+++ artecho_digital/onlinestore/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "561cdc4a4ed58d7497a2df0d1874cb65",
+const RESOURCES = {"flutter_bootstrap.js": "bb524109a5835169a66eac29cf656a09",
 "version.json": "88d9b95253bbbe6046bdb06cfa04a85c",
 "index.html": "4be7fc4724f720d11e23f5cf50b3547f",
 "/": "4be7fc4724f720d11e23f5cf50b3547f",
-"main.dart.js": "a2fca0d68b51a56a7dbae0a77b89ebfb",
+"main.dart.js": "ebb35b74c23243350ba0290e0c92f2df",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
