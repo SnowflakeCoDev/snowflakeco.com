ce105c7fcbec90f8bbd26720d11e6601f3830ead
SnowflakeCoDev <snowflakecodev12022@gmail.com>
2026-04-28 23:12:27 +0300

    Update

--- tech/digital/aiprompts/flutter_bootstrap.js
+++ tech/digital/aiprompts/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2776515619" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2387926225" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
